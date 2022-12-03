6d4cddc24f835e6a6e739eaaf99eadb034af7801
Bakr-Ali <49000030+Bakr-Ali@users.noreply.github.com>
2022-12-03 22:56:09 +0300

    Update compare_html_downloads.r

--- test/compare_html_downloads.r
+++ test/compare_html_downloads.r
@@ -1,9 +1,9 @@
 
 url <- "https://www.gsmarena.com/samsung_galaxy_a04e-11945.php"
 
-xml2::write_xml(read_html(url), file = "compare_html_downloads/read_html_0.html")
-xml2::write_xml(xml2::read_html(url), file = "compare_html_downloads/read_html_xml2.html")
-xml2::write_xml(rvest::read_html(url), file = "compare_html_downloads/read_html_rvest.html")
+xml2::write_xml(read_html(url), file = "read_html_0.html")
+xml2::write_xml(xml2::read_html(url), file = "read_html_xml2.html")
+xml2::write_xml(rvest::read_html(url), file = "read_html_rvest.html")
 xml2::download_xml(url)
 
 
@@ -16,7 +16,7 @@ my_save_html <- function(l_src, file_name) {
 }
 
 my_save_html(as.character(src), "read_html_xml2-write_lines_as_character.html")
-# my_save_html(toString(src), "read_html_xml2-write_lines_toString.html") # exactly the same as `as.char`
+my_save_html(toString(src), "read_html_xml2-write_lines_toString.html") # exactly the same as `as.char`
 
 # https://stackoverflow.com/a/55142671/9177407
   # cat(src, file = "output.html") # argument 1 (type 'list') cannot be handled by 'cat'
