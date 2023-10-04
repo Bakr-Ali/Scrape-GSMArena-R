afc888666b1203265e49783f5100c03784a43378
Bakr-Ali <49000030+Bakr-Ali@users.noreply.github.com>
2023-10-04 23:04:05 +0300

    Don't run cli vpn functions if not on Linux

--- gsma_tests.r
+++ gsma_tests.r
@@ -26,22 +26,30 @@ library(stringr)
 options(stringsAsFactors = FALSE)
 
 
-# TODO: don't run these vpn functions if not on Linux
+# don't run these vpn functions if not on Linux
 switch_vpn <- function(x = 10) {
-  print("Switching VPN server..")
-  protonvpn_resp <- system("protonvpn-cli c -r", intern = TRUE)
-  if (!grepl("Successfully connected", protonvpn_resp[4], fixed = TRUE)) {
-    disconnect_vpn(5)
-    switch_vpn()
+  if (.Platform$OS.type == "unix") {
+    print("Switching VPN server..")
+    protonvpn_resp <- system("protonvpn-cli c -r", intern = TRUE)
+    if (!grepl("Successfully connected", protonvpn_resp[4], fixed = TRUE)) {
+      disconnect_vpn(5)
+      switch_vpn()
+    }
+    print("VPN changed!")
+    Sys.sleep(x)
+  } else {
+    print("Not running on linux. No cli vpn.")
   }
-  print("VPN changed!")
-  Sys.sleep(x)
 }
 disconnect_vpn <- function(x = 1) {
-  print("Disconnecting VPN..")
-  protonvpn_resp <- system("protonvpn-cli d", intern = TRUE)
-  print("VPN Disconnected!")
-  Sys.sleep(x)
+  if (.Platform$OS.type == "unix") {
+    print("Disconnecting VPN..")
+    protonvpn_resp <- system("protonvpn-cli d", intern = TRUE)
+    print("VPN Disconnected!")
+    Sys.sleep(x)
+  } else {
+    print("Not running on linux. No cli vpn.")
+  }
 }
 
 safe_read_html <- function(url, x = 10) {
